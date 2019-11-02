15c47925b0f9024593d4a7c24e6cb6d2a62c380c
Hasan Berke Tekin <hbt19031@gmail.com>
2019-11-02 19:36:19 +0200

    templates folder and basic templates of home, matches, players and teams pages are uploaded

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -6,7 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "Hello, world!"
+    return render_template("home.html")
+
+@app.route("/matches")
+def matches_page():
+    return render_template("matches.html")
+    
+@app.route("/players")
+def players_page():
+    return render_template("players.html")
+    
+@app.route("/teams")
+def teams_page():
+    return render_template("teams.html")
+
 
 
 if __name__ == "__main__":
