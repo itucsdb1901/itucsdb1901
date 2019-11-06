22ebd24968bc4184a63e0610294d67eccdcaf79f
Hasan Berke Tekin <hbt19031@gmail.com>
2019-11-06 12:22:07 +0200

    dbinit modified

--- dbinit.py
+++ dbinit.py
@@ -1,14 +1,13 @@
 import os
 import sys
-
 import psycopg2 as dbapi2
 
 
-INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
-]
 
+INIT_STATEMENTS = [
+    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER, FIRST_NAME VARCHAR(10))",
+    "INSERT INTO DUMMY (NUM, FIRST_NAME) VALUES (45, 'HASAN')",
+]
 
 def initialize(url):
     with dbapi2.connect(url) as connection:
@@ -21,6 +20,5 @@ def initialize(url):
 if __name__ == "__main__":
     url = os.getenv("DATABASE_URL")
     if url is None:
-        print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
     initialize(url)
