f56cc9f56de19b9cf5033d5b978e67e630ff5278
Hasan Berke Tekin <hbt19031@gmail.com>
2019-11-06 17:54:59 +0200

    SQL connection chacnged

--- server.py
+++ server.py
@@ -1,19 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 import views
+import psycopg2 as dbapi2
+
 
 
 def create_app():
     app = Flask(__name__)
     app.add_url_rule("/", view_func=views.home_page)
+    app.add_url_rule("/add_player", methods=["POST"], view_func=views.add_player)
     app.add_url_rule("/players", view_func=views.players_page)
     app.add_url_rule("/teams", view_func=views.teams_page)
     app.add_url_rule("/matches", view_func=views.matches_page)
     app.add_url_rule("/leagues", view_func=views.leagues_page)
-    app.config['SQL_ALCHEMY_DATABASE_URI'] = 'postgres://postgres:docker@localhost:5432/football'
-    db = SQLAlchemy(app)
-
+    app.config['db_url'] = 'postgres://postgres:docker@localhost:5432/football'
+    #db = SQLAlchemy(app)
+    #connection = dbapi2.connect('postgres://postgres:docker@localhost:5432/football')
+    #app.config['db'] = db
     return app
+
 
 
 if __name__ == "__main__":
