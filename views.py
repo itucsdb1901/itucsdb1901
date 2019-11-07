1ceac6c469a066b3134116d7be6138e177ed548a
TalhaSelimhan <TalhaSelimhan@users.noreply.github.com>
2019-11-07 22:37:56 +0300

    Changed players page querys for new data base tables

--- views.py
+++ views.py
@@ -44,14 +44,14 @@ def matches_page():
 
 def player_page(personid):
     url = current_app.config["db_url"]
-    query = "SELECT * FROM test1 WHERE (id=%d)"%personid
+    query = "SELECT * FROM PERSON WHERE (id=%d)"%personid
     result=getOneRowQuery(url,query)
     person=classes.Person(id=int(result[0]),name=result[1],birthDay=1990,nationality="b")
     return render_template("player.html",player=person)
 
 def players_page():
     url = current_app.config['db_url']
-    listSQL = "SELECT * FROM test1"
+    listSQL = "SELECT * FROM PERSON"
     players = listTable(url, listSQL)
     return render_template("players.html", players=players)
     
