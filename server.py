6b0350b90381ce63d1d629cf677858fc4edc5d62
Hasan Berke Tekin <hbt19031@gmail.com>
2019-11-07 14:33:01 +0200

    New url rule for deleting player is added

--- server.py
+++ server.py
@@ -9,15 +9,13 @@ def create_app():
     app = Flask(__name__)
     app.add_url_rule("/", methods=["POST", "GET"], view_func=views.home_page)
     app.add_url_rule("/add_player", methods=["POST"], view_func=views.add_player)
-    app.add_url_rule("/players/<int:personid>", methods=["GET"], view_func=views.player_page)
+    app.add_url_rule("/players/detail_<int:personid>", methods=["POST","GET"], view_func=views.player_page)
+    app.add_url_rule("/player/delete_<int:personid>", methods=["POST"], view_func=views.delete_player)
     app.add_url_rule("/players", methods=["POST", "GET"], view_func=views.players_page)
     app.add_url_rule("/teams", view_func=views.teams_page)
     app.add_url_rule("/matches", view_func=views.matches_page)
     app.add_url_rule("/leagues", view_func=views.leagues_page)
     app.config['db_url'] = 'postgres://postgres:docker@localhost:5432/football'
-    #db = SQLAlchemy(app)
-    #connection = dbapi2.connect('postgres://postgres:docker@localhost:5432/football')
-    #app.config['db'] = db
     return app
 
 
